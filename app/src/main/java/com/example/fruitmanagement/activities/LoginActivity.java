5aed2e06de9e5b4ee962d4873cf30504f689b379
Thuy Phuong <thuyctpse150544@fpt.edu.vn>
2023-03-14 22:36:15 +0700

    Deploy socket server

--- app/src/main/java/com/example/fruitmanagement/activities/LoginActivity.java
+++ app/src/main/java/com/example/fruitmanagement/activities/LoginActivity.java
@@ -33,7 +33,7 @@ public class LoginActivity extends AppCompatActivity {
         if (remember) {
             String role = sharedPreferences.getString("Role", "");
 
-            Intent intent = new Intent(this, role.equals("User") ? ChatActivity.class : ChatActivity.class);
+            Intent intent = new Intent(this, role.equals("User") ? MainActivity.class : ChatActivity.class);
             startActivity(intent);
             finish();
             return;
@@ -66,7 +66,7 @@ public class LoginActivity extends AppCompatActivity {
         }
         String role = dto.getRole();
         if(role.equals("User")) {
-            Intent intent = new Intent(this, ChatActivity.class);
+            Intent intent = new Intent(this, MainActivity.class);
             intent.putExtra("DTO", dto);
             saveToPreference(dto);
             saveToPreference(isRemember);
